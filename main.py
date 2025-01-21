27b708dcf6ac35d1d46fbb8dcc8162fc4428dad9
sivavarunan <thevarasa.20221591@iit.ac.lk>
2025-01-21 15:16:41 +0530

    introduction to python

---
+++ main.py
@@ -0,0 +1,34 @@
+print("Hello World")
+
+# #is used for writing comment in python
+
+"""
+Long line of comment
+indentation is important in python
+Python is case sensitive
+"""
+
+# creating Variables
+x = 10
+deci = 10.212
+name = "siva"
+Name = 'eren'
+active = True
+
+"""
+variables doses not need to be declared with any particular type
+to specify datatype can be done using casting
+String variables can be declared using either single or double quotes
+variable names are case sensitive
+"""
+
+y = str(10)
+z = int(2)
+a = float(3)
+
+"""
+'print' is used to output in terminal
+'type' is used to get the data type of the variable
+"""
+
+print(type(y))       # output <class 'str'>
