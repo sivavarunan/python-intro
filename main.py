ca6652d3b42ba6a0c294b162b5d1eceb7744a1b2
sivavarunan <thevarasa.20221591@iit.ac.lk>
2025-01-21 15:30:42 +0530

    naming variables

--- main.py
+++ main.py
@@ -32,3 +32,22 @@ a = float(3)
 """
 
 print(type(y))       # output <class 'str'>
+
+"""
+There are some rules for variable names
+1. must start with letter or underscore character
+2. name cannot start with number
+3. only can contain (A-z , 0-9 and _ )
+4. names are case sensitive
+5. cannot be any python keywords
+Proper ways to write multi words variables names
+"""
+
+# 1.Camel Case - each word except the first, starts with capital
+myVariableName = "krishna"
+
+# 2.Pascal Case - each word start with capital letter
+MyVariableName = "krishna"
+
+# 3.Snake Case - each word is separated by underscore
+my_variable_name = "Krishna"
